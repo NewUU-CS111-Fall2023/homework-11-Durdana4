6b3ff74c0b2cb93a9ebe3b879e39f81ce9836ade
Durdana4 <127374217+Durdana4@users.noreply.github.com>
2024-01-23 20:18:13 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,20 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+
+using namespace std;
 
 class Problem7 {
 public:
+    int eulerFunction(int a, int b) {
+        return (a - 1) * (b - 1);
+    }
 };
+
+int main() {
+    Problem7 problem;
+    int a = 3;
+    int b = 5;
+    int result = problem.eulerFunction(a, b);
+    cout << "Euler function of " << a << " * " << b << ": " << result << endl;
+
+    return 0;
+}
