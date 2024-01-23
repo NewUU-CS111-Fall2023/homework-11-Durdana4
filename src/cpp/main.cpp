96d22e4adabd7501b7cf857ac6ba231272f5c223
Durdana4 <127374217+Durdana4@users.noreply.github.com>
2024-01-23 19:40:11 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,23 +1,12 @@
-/*
- USE THIS FILE FOR YOUR TESTS,
- AUTOGRADER WILL NOT EXECUTE THIS FILE.
- * Author:
- * Date:
- * Name:
- */
-
 #include <iostream>
 #include "task_1.cpp"
-#include "task_2.cpp"
-#include "task_3.cpp"
-#include "task_4.cpp"
-#include "task_5.cpp"
-#include "task_6.cpp"
-
-using namespace std;
-
-int main() {
-    // write your code here
-
-    return 0;
-}
+//#include "task_2.cpp"
+//#include "task_3.cpp"
+//#include "task_4.cpp"
+//#include "task_5.cpp"
+//#include "task_6.cpp"
+//#include "task_7.cpp"
+//#include "task_8.cpp"
+//#include "task_9.cpp"
+//#include "task_10.cpp"
+//#include "task_11.cpp"
